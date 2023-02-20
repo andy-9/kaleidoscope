67fa74d91eee7c352a238466bfd33ce4f93a6329
Andreas Hechler <info@andreashechler.com>
2023-02-20 18:20:38 +0100

    updated deploy-script

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ set -e
 set -x
 
 rsync --recursive --verbose --delete --exclude '.git' . andy@leo.uberspace.de:/home/andy/code/kaleidoscope
+ssh andy@leo.uberspace.de supervisorctl restart kaleidoscope
